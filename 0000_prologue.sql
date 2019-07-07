182c8bd8428f8245e220cbff4df4b7adc0bad29d
Feuerwehrsport Statistik <info@feuerwehrsport-statistik.de>
2019-07-07 03:42:02 +0200

    Backup 2019-07-07

--- 0000_prologue.sql
+++ 0000_prologue.sql
@@ -2317,4 +2317,4 @@ SELECT pg_catalog.setval('public.admin_users_id_seq', 94, true);
 -- Name: api_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.api_users_id_seq', 395, true);
+SELECT pg_catalog.setval('public.api_users_id_seq', 396, true);
