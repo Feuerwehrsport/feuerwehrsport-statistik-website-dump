545567a6fb6c59bde0538c2e1c6da3edf75cc530
Feuerwehrsport Statistik <info@feuerwehrsport-statistik.de>
2024-02-26 03:42:01 +0000

    Backup 2024-02-26

--- 0028_public.teams.sql
+++ 0028_public.teams.sql
@@ -3320,21 +3320,21 @@ COPY public.teams (id, name, shortcut, status, latitude, longitude, image, state
 -- Name: admin_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.admin_users_id_seq', 5808, true);
+SELECT pg_catalog.setval('public.admin_users_id_seq', 5822, true);
 
 
 --
 -- Name: api_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.api_users_id_seq', 766, true);
+SELECT pg_catalog.setval('public.api_users_id_seq', 767, true);
 
 
 --
 -- Name: appointments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.appointments_id_seq', 276, true);
+SELECT pg_catalog.setval('public.appointments_id_seq', 277, true);
 
 
 --
@@ -3348,7 +3348,7 @@ SELECT pg_catalog.setval('public.bla_badges_id_seq', 199, true);
 -- Name: change_logs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.change_logs_id_seq', 21712, true);
+SELECT pg_catalog.setval('public.change_logs_id_seq', 21713, true);
 
 
 --
@@ -3376,7 +3376,7 @@ SELECT pg_catalog.setval('public.competitions_id_seq', 2183, true);
 -- Name: delayed_jobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.delayed_jobs_id_seq', 29646, true);
+SELECT pg_catalog.setval('public.delayed_jobs_id_seq', 29666, true);
 
 
 --
@@ -3453,7 +3453,7 @@ SELECT pg_catalog.setval('public.m3_assets_id_seq', 14, true);
 -- Name: m3_logins_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.m3_logins_id_seq', 5802, true);
+SELECT pg_catalog.setval('public.m3_logins_id_seq', 5816, true);
 
 
 --
