63ac27ee85078fb0e1a2dcf06344ce281dca2a90
Feuerwehrsport Statistik <info@feuerwehrsport-statistik.de>
2024-03-18 03:42:02 +0000

    Backup 2024-03-18

--- 0028_public.teams.sql
+++ 0028_public.teams.sql
@@ -3320,14 +3320,14 @@ COPY public.teams (id, name, shortcut, status, latitude, longitude, image, state
 -- Name: admin_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.admin_users_id_seq', 6483, true);
+SELECT pg_catalog.setval('public.admin_users_id_seq', 6511, true);
 
 
 --
 -- Name: api_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.api_users_id_seq', 771, true);
+SELECT pg_catalog.setval('public.api_users_id_seq', 772, true);
 
 
 --
@@ -3348,14 +3348,14 @@ SELECT pg_catalog.setval('public.bla_badges_id_seq', 199, true);
 -- Name: change_logs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.change_logs_id_seq', 21731, true);
+SELECT pg_catalog.setval('public.change_logs_id_seq', 21733, true);
 
 
 --
 -- Name: change_requests_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.change_requests_id_seq', 527, true);
+SELECT pg_catalog.setval('public.change_requests_id_seq', 528, true);
 
 
 --
@@ -3376,7 +3376,7 @@ SELECT pg_catalog.setval('public.competitions_id_seq', 2183, true);
 -- Name: delayed_jobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.delayed_jobs_id_seq', 30439, true);
+SELECT pg_catalog.setval('public.delayed_jobs_id_seq', 30470, true);
 
 
 --
@@ -3453,7 +3453,7 @@ SELECT pg_catalog.setval('public.m3_assets_id_seq', 14, true);
 -- Name: m3_logins_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.m3_logins_id_seq', 6477, true);
+SELECT pg_catalog.setval('public.m3_logins_id_seq', 6505, true);
 
 
 --
