bc193efb71112e189a50130f02809b2d18ab75ab
Feuerwehrsport Statistik <info@feuerwehrsport-statistik.de>
2024-12-15 03:42:02 +0000

    Backup 2024-12-15

--- 0028_public.teams.sql
+++ 0028_public.teams.sql
@@ -3399,7 +3399,7 @@ COPY public.teams (id, name, shortcut, status, latitude, longitude, image, state
 -- Name: admin_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.admin_users_id_seq', 13940, true);
+SELECT pg_catalog.setval('public.admin_users_id_seq', 13960, true);
 
 
 --
@@ -3448,7 +3448,7 @@ SELECT pg_catalog.setval('public.competitions_id_seq', 2278, true);
 -- Name: delayed_jobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.delayed_jobs_id_seq', 45534, true);
+SELECT pg_catalog.setval('public.delayed_jobs_id_seq', 45558, true);
 
 
 --
@@ -3525,7 +3525,7 @@ SELECT pg_catalog.setval('public.m3_assets_id_seq', 14, true);
 -- Name: m3_logins_id_seq; Type: SEQUENCE SET; Schema: public; Owner: feuerwehrsport-statistik
 --
 
-SELECT pg_catalog.setval('public.m3_logins_id_seq', 13934, true);
+SELECT pg_catalog.setval('public.m3_logins_id_seq', 13954, true);
 
 
 --
